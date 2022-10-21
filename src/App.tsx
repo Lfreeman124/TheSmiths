e71f27e3864802f3495aba5d40f8bf48f11c5c41
Lauren Freeman <lfreeman124@gmail.com>
2022-10-21 14:01:38 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-// import MasterRouter from "./components/MasterRouter";
+import MasterRouter from "./components/MasterRouter";
 
 function App() {
   return (
     <div className="App">
       <h1>Hahaha I did it and it only cost $18. I win!</h1>
-      {/* <MasterRouter /> */}
+      <MasterRouter />
     </div>
   );
 }
