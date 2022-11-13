e18c06902ce228ed2477aac230b5105b8e1760b7
Lauren Freeman <lfreeman124@gmail.com>
2022-11-14 00:11:06 +0100

    countdown

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -74,7 +74,7 @@ const Home: React.FC = () => {
             </nav>
           )}
           <img src={us} alt=""></img>
-          {/* <Countdown /> */}
+          <Countdown />
         </section>
         <div className="button-container">
           <Link to="/rsvp">
