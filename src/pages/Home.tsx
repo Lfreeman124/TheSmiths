82badcf9bbeed39e24ac92f794e4544e7d30a3f3
Lauren Freeman <lfreeman124@gmail.com>
2022-11-14 14:22:25 +0100

    reveal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,7 +23,7 @@ const Home: React.FC = () => {
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
-      if (elementTop < windowHeight - 250) {
+      if (elementTop < windowHeight - 150) {
         reveals[i].classList.add("active");
       } else {
         reveals[i].classList.remove("active");
